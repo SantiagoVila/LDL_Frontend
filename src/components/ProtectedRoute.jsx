9490c2a78766aa1f33455d0574eeb8973fcd75bb
SantiagoVila <vilasantiago16@gmail.com>
2025-07-18 09:03:38 -0300

    fix: Corregir ruta de importación en ProtectedRoute

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,6 +1,6 @@
 // src/components/ProtectedRoute.jsx
 import { Navigate } from 'react-router-dom';
-import { useAuth } from '../context/AuthContext';
+import { useAuth } from '../Context/AuthContext';
 
 // Este componente recibe como 'hijo' la página que queremos proteger
 function ProtectedRoute({ children }) {
